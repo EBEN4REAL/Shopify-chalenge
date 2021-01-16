a28877a2349f0694d5f87330b86d607bc2f5c4af
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-01-16 22:55:21 +0100

    ..

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
- (function () {
+ (function searchMovies () {
     document.querySelector(".search-movies").addEventListener('input', (e) => {
         document.querySelector(".search_text").textContent = e.target.value
         const getMovies = async () => {
-            const response = await fetch(`https://www.omdbapi.com/?s=${e.target.value}&apikey=4a3b711b`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            }).then(res => {
-                console.log(res.data)
-            }).catch(e => {
-                console.log(e)
+            const response = await  fetch(`https://www.omdbapi.com/?s=${e.target.value}&apikey=4a3b711b`)
+            .then(response => {
+              return response.json();
             })
+            .then(jsonResponse => {
+
+            });
+            return response
         }
-        getMovies()
+        let movies = getMovies()
+        return movies
     })
  })()
+
+
+ console.log(searchMovies)
+
