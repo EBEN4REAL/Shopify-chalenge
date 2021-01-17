83e5ae6527f1e1162af1afe88c9aee23f4a43d6e
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-01-17 19:38:20 +0100

    ..

--- js/index.js
+++ js/index.js
@@ -1,21 +1,26 @@
- (function () {
+this.state = {
+    movies: [],
+    nominatedMovies: []
+};
+
+(function () {
     document.querySelector(".search-movies").addEventListener('input', (e) => {
-        console.log(e.target.value)
         document.querySelector(".search_text").textContent = e.target.value
         const getMovies = async () => {
             const response = await  fetch(`https://www.omdbapi.com/?s=${e.target.value}&apikey=4a3b711b`)
             .then(response => {
               return response.json();
             })
-            .then(jsonResponse => {
-                if(jsonResponse.Response == 'True') {
-                    jsonResponse.Search.forEach(el => {
+            .then(movies => {
+                console.log(movies.Search)
+                if(movies.Response == 'True') {
+                    movies.Search.forEach(el => {
                         el.nominated = false
                     })
                     document.querySelector('.movies-list').innerHTML = '';
                     let parentDiv = document.createElement('div')
                     parentDiv.classList.add('movie__list')
-                    jsonResponse.Search.forEach(el => {
+                    movies.Search.forEach(el => {
                         let results_wrapper = document.createElement('div')
                         results_wrapper.classList.add('results_wrapper') 
                         results_wrapper.classList.add('mt-2')
@@ -28,12 +33,26 @@
                         let search_action  = document.createElement('div')
                         search_action.classList.add('search_action')
                         let nominate_button = document.createElement('button')
+                        nominate_button.classList.add('nominate-movie')
+                        nominate_button.setAttribute('data-imdb' , el.imdbID)
                         nominate_button.textContent = `${!el.nominated ? 'Nominate' : "Nominated"} `
+                        nominate_button.addEventListener('click', (e) => {
+                            let nominatedMovie = this.state.nominatedMovies.findIndex(mv => mv.imdbID === el.imdbID) 
+                            console.log(el)
+                            if(nominatedMovie === -1) {
+                                window.state.nominatedMovies.push(el)
+                                e.target.classList.add('disabled-button')
+                            }
+                            console.log(this.state.nominatedMovies)
+                        })
                         search_action.append(nominate_button)
-
                         results_wrapper.append(movie_title, search_action)
                         parentDiv.appendChild(results_wrapper)
                         document.querySelector('.movies-list').append(parentDiv)
+                    })
+                    this.state.movies = movies.Search
+                    this.state.nominatedMovies.forEach(nominatedMobie => {
+                        
                     })
                 }else {
                     document.querySelector('.movies-list').innerHTML = '';
@@ -43,6 +62,7 @@
        getMovies()
     })
  })()
+
 
 
 
